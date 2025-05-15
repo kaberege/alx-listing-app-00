8d313c75a732a93a26a0235b62e233ac9f4f33c8
kaberege <kabgnestor@gmail.com>
2025-05-15 19:06:33 +0200

    added content to the components

---
+++ components/common/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ButtonProps } from '../../interfaces';
+
+const Button: React.FC<ButtonProps> = ({ label, onClick, type = 'button' }) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,3 @@
 export const arr = [1,2,3,4,5,6,7];
+export const API_BASE_URL = 'https://api.example.com';
+export const PLACEHOLDER_IMAGE = '/assets/placeholder.jpg';
